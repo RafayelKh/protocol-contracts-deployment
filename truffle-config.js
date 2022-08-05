2061408bc4e5379bf1add1ce784dd88742271bb0
DevHakob <dev.arshakyan@gmail.com>
2022-08-05 21:44:17 +0400

    config for deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,54 +1,55 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const os = require('os');
 
-let apiKey;
-try {
-	console.log(`Loading etherscan key from ${os.homedir() + "/.ethereum/etherscan.json"}`);
-	apiKey = require(os.homedir() + "/.ethereum/etherscan.json").apiKey;
-	console.log("loaded etherscan api key");
-} catch {
-	console.log("unable to load etherscan key from config")
-	apiKey = "UNKNOWN"
-}
+// let apiKey;
+// try {
+// 	console.log(`Loading etherscan key from ${os.homedir() + "/.ethereum/etherscan.json"}`);
+// 	apiKey = require(os.homedir() + "/.ethereum/etherscan.json").apiKey;
+// 	console.log("loaded etherscan api key");
+// } catch {
+// 	console.log("unable to load etherscan key from config")
+// 	apiKey = "UNKNOWN"
+// }
 
-function createNetwork(name) {
-  try {
-    var json = require(os.homedir() + "/.ethereum/" + name + ".json");
-    var gasPrice = json.gasPrice != null ? json.gasPrice : 2000000000;
+// function createNetwork(name) {
+//   try {
+//     var json = require(os.homedir() + "/.ethereum/" + name + ".json");
+//     var gasPrice = json.gasPrice != null ? json.gasPrice : 2000000000;
 
-    return {
-      provider: () => {
-        const { estimate } = require("@rarible/estimate-middleware")
-	      if (json.path != null) {
-	        const { createProvider: createTrezorProvider } = require("@rarible/trezor-provider")
-	        const provider = createTrezorProvider({ url: json.url, path: json.path, chainId: json.network_id })
-	        provider.send = provider.sendAsync
-	        return provider
-	      } else {
-	        return createProvider(json.address, json.key, json.url)
-	      }
-      },
-      from: json.address,
-      gas: 6000000,
-      gasPrice: gasPrice + "000000000",
-      network_id: json.network_id,
-      skipDryRun: true,
-      networkCheckTimeout: 500000
-    };
-  } catch (e) {
-    return null;
-  }
-}
+//     return {
+//       provider: () => {
+//         const { estimate } = require("@rarible/estimate-middleware")
+// 	      if (json.path != null) {
+// 	        const { createProvider: createTrezorProvider } = require("@rarible/trezor-provider")
+// 	        const provider = createTrezorProvider({ url: json.url, path: json.path, chainId: json.network_id })
+// 	        provider.send = provider.sendAsync
+// 	        return provider
+// 	      } else {
+// 	        return createProvider(json.address, json.key, json.url)
+// 	      }
+//       },
+//       from: json.address,
+//       gas: 6000000,
+//       gasPrice: gasPrice + "000000000",
+//       network_id: json.network_id,
+//       skipDryRun: true,
+//       networkCheckTimeout: 500000
+//     };
+//   } catch (e) {
+//     return null;
+//   }
+// }
 
-function createProvider(address, key, url) {
-  console.log("creating provider for address: " + address);
-  var HDWalletProvider = require("@truffle/hdwallet-provider");
-  return new HDWalletProvider(key, url);
-}
+// function createProvider(address, key, url) {
+//   console.log("creating provider for address: " + address);
+//   var HDWalletProvider = require("@truffle/hdwallet-provider");
+//   return new HDWalletProvider(key, url);
+// }
 
 module.exports = {
-	api_keys: {
-    etherscan: apiKey
-  },
+	// api_keys: {
+  //   etherscan: apiKey
+  // },
 
 	plugins: [
     'truffle-plugin-verify',
@@ -56,17 +57,25 @@ module.exports = {
   ],
 
   networks: {
-    e2e: createNetwork("e2e"),
-    ops: createNetwork("ops"),
-    ropsten: createNetwork("ropsten"),
-    mainnet: createNetwork("mainnet"),
-    rinkeby: createNetwork("rinkeby"),
-    rinkeby2: createNetwork("rinkeby2"),
-    polygon_mumbai: createNetwork("polygon_mumbai"),
-    polygon_mainnet: createNetwork("polygon_mainnet"),
-    polygon_dev: createNetwork("polygon_dev"),
-    dev: createNetwork("dev"),
-    goerli: createNetwork("goerli")
+    // e2e: createNetwork("e2e"),
+    // ops: createNetwork("ops"),
+    // ropsten: createNetwork("ropsten"),
+    // mainnet: createNetwork("mainnet"),
+
+    rinkeby: {
+      provider: () => new HDWalletProvider('blouse price start often ethics aim slender broccoli solution impose drill devote', 'wss://eth-rinkeby.alchemyapi.io/v2/fo1n0I2MhJV5blUzy557KP8CMuKN3YSX'),
+      network_id: 4,
+      gas: 600000000,
+      // confirmations: 2,
+      timeoutBlocks: 200,
+      // skipDryRun: true
+    },
+    // rinkeby2: createNetwork("rinkeby2"),
+    // polygon_mumbai: createNetwork("polygon_mumbai"),
+    // polygon_mainnet: createNetwork("polygon_mainnet"),
+    // polygon_dev: createNetwork("polygon_dev"),
+    // dev: createNetwork("dev"),
+    // goerli: createNetwork("goerli")
   },
 
   compilers: {
